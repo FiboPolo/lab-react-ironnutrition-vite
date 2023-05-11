7cfd27bd0d6f30ba22aac25f4ce7c63740219ae8
Steve PB <spoloboyd@gmail.com>
2023-05-11 16:34:51 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import foodsFromJSON from "./foods.json";
+import React, { useState } from 'react'
+import FoodBox from './components/FoodBox';
+import { Col, Row, Divider, Button, Layout } from 'antd';
+import AddFoodForm from './components/AddFoodForm';
+import OnSearch from './components/Search';
+import { findByLabelText } from '@testing-library/react';
+import EmptyList from './components/EmptyList';
+const { Content } = Layout;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [foods, setFoods] = useState(foodsFromJSON)
+  const foodsBackUp = foodsFromJSON
+
+
+  const handleAddFood = (newFood) => {
+    setFoods([...foods, newFood]);
+  };
+
+  const filterFoodList = (str) => {
+    let filteredFoods;
+    if (str === '') {
+      filteredFoods = foodsBackUp;
+    } else {
+      filteredFoods = foodsBackUp.filter((food) => {
+        return food.name.toLowerCase().includes(str.toLowerCase());
+      });
+    }
+    setFoods(filteredFoods);
+  };
+
+  const deleteFood = (foodToDelete) => {
+    const newFoods = foods.filter(food => {
+      return food !== foodToDelete
+    });
+    setFoods(newFoods);
+  };
+
+  const contentStyle = {
+    display: 'flex',
+    wrap: 'wrap',
+    justifyContent: 'space-around',
+    textAlign: 'center',
+    //minHeight: 120,
+    lineHeight: '120px',
+    //color: '#fff',
+    backgroundColor: '#108ee9',
+    maxWidth: '80%',
+    margin: '0 150px'
+  };
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+        <Row>
+          <Col span={14} offset={5}>
+            <AddFoodForm addFood={handleAddFood} />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={14} offset={5}>
+            <OnSearch onSearch={filterFoodList}/>
+          </Col>
+        </Row>
+      <Divider>Food List</Divider>
+        <Layout>       
+          <Content style={contentStyle}>
+          {/* {!deleteFood.length && <EmptyList />} */}
+            <Row style={{ width: '80%', justifyContent: 'space-around' }}>
+              {/* <Col span={14} offset={5}> */}
+                {foods.map((food) => (
+              <FoodBox key={food.name} food={food} deleteFood={() => deleteFood(food)} />
+                ))}
+              {/* </Col> */}
+            </Row>
+          </Content>
+        </Layout>
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+      </Row>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/FoodBox.jsx
@@ -0,0 +1,25 @@
+import { Card, Col, Button } from 'antd';
+
+// Iteration 2
+function FoodBox(props) {
+    const { food, deleteFood } = props
+    const { foodToDelete } = props
+    return (
+    <Col>
+      <Card
+        title={food.name}
+        style={{ width: 230, margin: 10 }}
+      >
+        <img src={food.image} height={60} alt="food" />
+        <p>Calories: {food.calories}</p>
+        <p>Servings: {food.servings}</p>
+        <p>
+          <b>Total Calories: {food.calories} </b> kcal
+        </p>
+        <Button type="primary" onClick={() => deleteFood(food)}> Delete </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;
